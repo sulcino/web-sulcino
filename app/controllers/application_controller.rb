b80e43c4b9a83aec3e81899f1b9cee5f78f6d89c
David Rak <rak@jchsoft.cz>
2025-02-22 02:34:17 +0100

    prepared for localisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_locale
+
+  private
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def set_locale
+    if params[:locale].present? && I18n.available_locales.include?(params[:locale].to_sym)
+      I18n.locale = params[:locale]
+      session[:locale] = I18n.locale
+    elsif session[:locale].present?
+      I18n.locale = session[:locale]
+    else
+      I18n.locale = I18n.default_locale
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  root "home_page#index"
+
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    # Defines the root path route ("/")
+    root "home_page#index"
+  end
 end
